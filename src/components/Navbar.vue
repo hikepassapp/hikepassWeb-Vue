3a9731e919d068471509e55b03c813ccaa0be16c
Dewanta Rahma Satria <dewantarahmasatria@gmail.com>
2025-11-07 09:40:23 +0700

    Install Bootstrap + install rooter

---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <a class="navbar-brand" href="#">HikePass</a>
+
+    <button 
+      class="navbar-toggler" 
+      type="button" 
+      data-bs-toggle="collapse" 
+      data-bs-target="#nav"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="nav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Reservasi</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Tiket Saya</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+
+    <section class="hero">
+      <h1>Selamat datang di HikePass!</h1>
+      <p>Platform pendakian modern ✨</p>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+.hero {
+  padding: 50px;
+}
+</style>
